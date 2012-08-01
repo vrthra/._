d19a0d3645b7cd3db9a1c8c4c501fc46caa65b1b
rahul <9@vrtra.net>
2012-07-31 17:02:57 -0700

    Add term info compiler.

---
+++ bin/compile-terminfo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+case $1 in
+  -h) cat <<EOF
+`basename $0` -e
+  Used to create a term info file in the home directory if it is not found"
+EOF
+    exit 0
+    ;;
+  -e) wget http://www.catb.org/~esr/terminfo/termtypes.ti.gz -O - | gzip -dc > termtypes.ti;
+      tic -o ~/ termtypes.ti
+      rm termtypes.ti
+    ;;
+esac
