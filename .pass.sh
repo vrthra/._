7dcbd2381feb06de80318e09df35dd64c0b51484
rahul <rahul@puppetlabs.com>
2012-08-18 16:13:55 -0700

    pass: add password management.

---
+++ .pass.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo -n "Name:"
+read name
+echo -n "Password:"
+read password
+echo -n "Email:"
+read email
+
+cat <<'EOF' | sed -e "s/%name%/$name/g"\
+                  -e "s/%password%/$password/g" \
+                  -e "s/%email%/$email/g" \
+                  > ~/.pass
+git_name=%name%
+git_password=%password%
+git_email=%email%
+EOF
