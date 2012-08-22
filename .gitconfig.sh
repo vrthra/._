23a563ce2edd82bd10c64b703758161c4a8212e6
rahul <rahul@puppetlabs.com>
2012-08-22 10:57:43 -0700

    git: update config

--- .gitconfig.sh
+++ .gitconfig.sh
@@ -19,6 +19,10 @@ cat <<EOF
   graph = log --graph --date-order -C -M --pretty=format:\"<%h> %ad [%an] %Cgreen%d%Creset %s\" --all --date=short
   this = !git branch | sed -ne '/^ *\\*/p' | cut -f 2 -d' '
   amend = commit --amend
+  xo = "!f() { git log ..o/$1; }; f"
+  ox = "!f() { git log o/$1..; }; f"
+  xi = !git log ..i/`git mybranch`
+  ix = !git log i/`git mybranch`..
 
 [color]
   diff = auto
