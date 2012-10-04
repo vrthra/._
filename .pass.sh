d6dba0619732bc1192e501f4ab25152b6ae153bd
Rahul Gopinath <8@vrtra.net>
2012-10-04 09:28:11 -0700

    better pass

--- .pass.sh
+++ .pass.sh
@@ -4,28 +4,24 @@ readx() {
   local v var val
   var=$2
   val=$(eval echo \$$var)
-  echo -n "$1($val):"
+  echo -n "  $1($val):"
   read v
   case x$v in
     x) ;;
-    *) eval "$var='$v'";;
+    *) val=$v
   esac
+  echo " $var='$val'" >> ~/.pass.tmp
 }
 
 [ -e ~/.pass ] && . ~/.pass || :
 
 echo > ~/.pass.tmp
 
-readx "Git:Name" git_name
-readx "Git:Password" git_password
-readx "Git:Email" git_email
-
-cat <<EOF >> ~/.pass.tmp
-# git:
-  git_name="$git_name"
-  git_password="$git_password"
-  git_email="$git_email"
-
-EOF
+echo "# git:" | tee -a ~/.pass.tmp
+readx "Name" git_name
+readx "Password" git_password
+readx "Email" git_email
+echo
 
 cat ~/.pass.tmp > ~/.pass
+~/.gitconfig.sh
